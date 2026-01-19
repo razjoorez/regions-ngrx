a47adf59eab7c5b8a6b92056d1fa86175c7d42cf
Reza Razjoo <reza.razjoo@yahoo.com>
2026-01-19 01:12:40 +0000

    claude tests for components

--- src/app/country-details/country-details.component.spec.ts
+++ src/app/country-details/country-details.component.spec.ts
@@ -1,17 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { CountryDetailsComponent } from './country-details.component';
+import { RegionState } from '../state/region.reducer';
 
 describe('CountryDetailsComponent', () => {
   let component: CountryDetailsComponent;
   let fixture: ComponentFixture<CountryDetailsComponent>;
+  let store: MockStore;
+
+  const initialState: { regions: RegionState } = {
+    regions: {
+      region1: 'Asia',
+      region2: 'Europe',
+      regionInit: ['Asia', 'Europe'],
+      regionSelected: 'Europe',
+      countries: [],
+      countrySelected: {
+        name: 'Germany',
+        capital: 'Berlin',
+        population: '83000000',
+        currencies: [{ name: 'Euro' }],
+        flag: 'https://example.com/germany.svg'
+      },
+      loading: false,
+      error: null
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CountryDetailsComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [CountryDetailsComponent],
+      providers: [provideMockStore({ initialState })]
+    }).compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(CountryDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/regions/regions.component.spec.ts
+++ src/app/regions/regions.component.spec.ts
@@ -1,17 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { RegionsComponent } from './regions.component';
+import { RegionState } from '../state/region.reducer';
 
 describe('RegionsComponent', () => {
   let component: RegionsComponent;
   let fixture: ComponentFixture<RegionsComponent>;
+  let store: MockStore;
+
+  const initialState: { regions: RegionState } = {
+    regions: {
+      region1: 'Asia',
+      region2: 'Europe',
+      regionInit: ['Asia', 'Europe'],
+      regionSelected: '',
+      countries: [],
+      countrySelected: {
+        name: '',
+        capital: '',
+        population: '',
+        currencies: [{ name: '' }],
+        flag: ''
+      },
+      loading: false,
+      error: null
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegionsComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [RegionsComponent],
+      providers: [provideMockStore({ initialState })]
+    }).compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(RegionsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
